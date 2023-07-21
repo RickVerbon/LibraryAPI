a372a300bad417e17826c24043e64acbaabed915
Rick Verbon <rick89@gmail.com>
2023-07-21 22:14:29 +0200

    Changed model

--- api/models.py
+++ api/models.py
@@ -12,19 +12,6 @@ class Author(models.Model):
         return self.name
 
 
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
-    pages = models.IntegerField()
-    isbn = models.CharField(max_length=100)
-    language = models.CharField(max_length=100)
-    category = models.ManyToManyField('Category', related_name='books')
-    is_available = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.title
-
-
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
@@ -32,9 +19,22 @@ class Category(models.Model):
         return self.name
 
 
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    pages = models.IntegerField()
+    isbn = models.CharField(max_length=100)
+    language = models.CharField(max_length=100)
+    category = models.ManyToManyField(Category, related_name='books')
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
 class Reservation(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reservations')
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='reservations')
+    user = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='reservations')
     reservation_date = models.DateTimeField(auto_now_add=True)
     return_date = models.DateTimeField(blank=True, null=True)
 
